8e35b003ebb930315114830c3099ca2886caf510
rob0302 <rob0302@knighrs.ucf.edu>
2021-11-29 20:25:39 -0500

    first commit

---
+++ EX4.cpp
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Robert McQueen
+ */
+#include "std_lib_facilities.h"
+#include <vector>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+class Name_Value{
+String name;
+int score;
+int people =0;
+void set_Value(String,int);
+vector<Name_Value> NameVal;
+
+};
+
+void Name_Value :: set_Value (String name, int score){
+cin>>name>>score;
+
+}
